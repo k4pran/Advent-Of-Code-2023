4f27c02ce9db2e38f6cfb684fa1b6d8b811ccdfd
k4pran <k4pran@gmail.com>
2023-12-01 18:28:41 +0000

    Day1 - cleanup

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 number_names = [
     "one",
     "two",
@@ -10,28 +12,30 @@ number_names = [
     "nine"
 ]
 
-def get_first_number(line: str):
-    for i in range(0, len(line)):
-        if line[i].isdigit():
-            return line[i]
-        for number_name in number_names:
-            if number_name in line[0:i + 1]:
-                return str(number_names.index(number_name) + 1)
 
-def get_last_number(line: str):
-    for i in reversed(range(0, len(line))):
+def find_number(i: int, substring_indexing_fn: Callable):
+    for number_name in number_names:
+        if number_name in substring_indexing_fn(line, i):
+            return str(number_names.index(number_name) + 1)
+
+
+def get_boundary_number(line: str, index_range: [range, reversed], substring_indexing_fn: Callable):
+    for i in index_range:
         if line[i].isdigit():
             return line[i]
-        for number_name in number_names:
-            if number_name in line[i:]:
-                return str(number_names.index(number_name) + 1)
+
+        name_as_number = find_number(i, substring_indexing_fn)
+        if name_as_number:
+            return name_as_number
+
 
 with open("day1.txt", 'r') as f:
     lines = f.read().splitlines()
 
     total = 0
     for line in lines:
-        digits = [get_first_number(line), get_last_number(line)]
+        digits = [get_boundary_number(line, range(0, len(line)), lambda l, i: l[:i + 1]),
+                  get_boundary_number(line, reversed(range(0, len(line))), lambda l, i: l[i:])]
         number = digits[0] + digits[-1]
         total += int(number)
 
