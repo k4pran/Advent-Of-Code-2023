871712c1539b0140f4c4b5fd695ceeeb59d5389a
k4pran <k4pran@gmail.com>
2023-12-02 07:25:03 +0000

    Day 2

---
+++ day2/day2-1.py
@@ -0,0 +1,44 @@
+max_values = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+def parse_cube_set(cube_set):
+    color_values = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+    for color_part in cube_set.split(","):
+        _, count, color = color_part.split(" ")
+        color_values[color] = int(count)
+    return color_values
+
+def parse_line(line):
+    game_part, cubes_part = line.split(':')
+    game_id = game_part.split(' ')[-1]
+    cube_sets = cubes_part.split(";")
+
+    totals = {
+        'red': 0,
+        'blue': 0,
+        'green': 0
+    }
+    for cube_set in cube_sets:
+        color_counts = parse_cube_set(cube_set)
+        for color, count in color_counts.items():
+            totals[color] += count
+
+            if count > max_values[color]:
+                return 0
+    return int(game_id)
+
+with open("day2.txt", 'r') as f:
+    lines = f.read().splitlines()
+
+    total = 0
+    for line in lines:
+        total += parse_line(line)
+
+    print(f"Day 2-1: {total}")
