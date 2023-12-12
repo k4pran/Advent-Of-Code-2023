f1da7f3c24f623e34ca6651fdff3705d4518b287
k4pran <k4pran@gmail.com>
2023-12-12 17:48:41 +0000

    Day 11 finally a good solution!

--- day11/day11-2.py
+++ day11/day11-2.py
@@ -1,12 +1,11 @@
 import itertools
 
-import numpy as np
 
-def expand_universe(lines):
-    mat = []
-
+def expand_universe(lines, expansion=1000000 - 1):
     empty_galaxy_rows = set(i for i in range(len(lines)))
     empty_galaxy_cols = set(i for i in range(len(lines[0])))
+
+    galaxies = []
     for i, row in enumerate(lines):
         new_row = []
         for j, col in enumerate(row):
@@ -14,42 +13,47 @@ def expand_universe(lines):
             if col == "#":
                 empty_galaxy_rows.discard(i)
                 empty_galaxy_cols.discard(j)
-        mat.append(new_row)
+                galaxies.append([i, j])
+
+    empty_galaxy_cols = sorted([i for i in empty_galaxy_cols])
+    empty_galaxy_rows = sorted([i for i in empty_galaxy_rows])
 
     inc = 0
-    for row in empty_galaxy_rows:
-        for i in range(1000000):
-            mat.insert(row + inc, list(mat[row + inc]))
-            inc += 1
-
+    galaxies = sorted(galaxies, key=lambda x: x[1])
+    for i, empty_col in enumerate(empty_galaxy_cols):
+        for j, galaxy in enumerate(galaxies):
+            if galaxy[1] > empty_col:
+                for galaxy_to_update in galaxies[j:]:
+                    galaxy_to_update[1] += inc + expansion
+                for k, empty_galaxy_col in enumerate(empty_galaxy_cols[i + 1:]):
+                    empty_galaxy_cols[k + i + 1] += inc + expansion
+                break
 
     inc = 0
-    for col in empty_galaxy_cols:
-        for row in mat:
-            for i in range(1000000):
-                row.insert(col + inc, row[col + inc])
-        inc += 1
+    galaxies = sorted(galaxies, key=lambda x: x[0])
+    for i, empty_rows in enumerate(empty_galaxy_rows):
+        for j, galaxy in enumerate(galaxies):
+            if galaxy[0] > empty_rows:
+                for galaxy_to_update in galaxies[j:]:
+                    galaxy_to_update[0] += inc + expansion
+                for k, empty_galaxy_row in enumerate(empty_galaxy_rows[i + 1:]):
+                    empty_galaxy_rows[k + i + 1] += inc + expansion
+                break
 
-    # yuk
-    galaxies = []
-    for i, row in enumerate(mat):
-        for j, col in enumerate(row):
-            if col == "#":
-                galaxies.append((i, j))
+    return galaxies
 
-    return mat, galaxies
 
 def manhattan_dist(p1, p2):
     return abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
 
+
 def find_path_total(lines):
-    mat, galaxies, = expand_universe(lines)
+    galaxies = expand_universe(lines)
 
     total_dist = 0
     for pairing in itertools.combinations(galaxies, 2):
         dist = manhattan_dist(pairing[0], pairing[1])
         total_dist += dist
-        # print(str(pairing) + " -- " + str(dist))
     return total_dist
 
 
